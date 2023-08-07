1d2142ec01f36b6be29593671707dfbf308a1d4c
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-08-07 14:04:01 +0300

    Final Updates

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ function randomVerse(){
             'X-RapidAPI-Host': 'bible-search.p.rapidapi.com'
 	}})
         .then(response => response.json())
-        .then(respData => 
-            firstVerse(respData)
-            )
+        .then(respData => firstVerse(respData))
         .catch(error => {
             alert("Sorry Can't Display Verse!")
             console.log(error.message)
@@ -22,18 +20,31 @@ function randomVerse(){
 }
 
 function firstVerse(verseData){
+    const book_name = verseData[0].book_name
+    const chapter = verseData[0].chapter
+    const verse = verseData[0].verse
+    const version = verseData[0].translation_id
+    const bookDetails = document.querySelector("p")
     const displayVerse = document.querySelector("#random")
+
+    bookDetails.innerText = `${book_name} ${chapter} : ${verse} ${version}`
     displayVerse.innerText = verseData[0].text
+
 }
 
 document.addEventListener('DOMContentLoaded',randomVerse())
 
 // Content Display
-function searchData(){
-    const bookName = document.querySelector("#searchinput").value
-    const chapter = document.querySelector("#chapter").value
 
-    const chapterUrl = `https://bible-memory-verse-flashcard.p.rapidapi.com/get_chapter?book_name=${bookName}&chapter=${chapter}&text_mode=full`
+    const bookName = document.querySelector("#searchinput")
+    const chapter = document.querySelector("#chapter")
+    const verse = document.querySelector("#verse")
+    const bibleContent = document.getElementById('bibleContent')
+    const quotedVerse = document.getElementById('quotedVerse')
+
+function searchData(){
+
+    const chapterUrl = `https://bible-memory-verse-flashcard.p.rapidapi.com/get_chapter?book_name=${bookName.value}&chapter=${chapter.value}&text_mode=full`
 
  return chapterUrl
 }
@@ -49,10 +60,9 @@ function bookHandler(url){
     })
         .then(response => response.json())
         .then(respData => {
-            const verse = document.querySelector("#verse").value
-            const quotedVerse = document.getElementById('quotedVerse')
-
-            return quotedVerse.innerText = respData.chapter[verse-1].verse_text
+            console.log(respData)
+            bibleContent.innerText = `${respData.book_name} ${respData.chapter[chapter.value].chapter} : ${respData.chapter[verse.value-1].verse_num} `
+            quotedVerse.innerText = respData.chapter[verse.value-1].verse_text
         })
         .catch(error => {
             alert("Server Error!")
@@ -61,12 +71,12 @@ function bookHandler(url){
 
 }
 
-function bibleContent(event){
+function contentDisplay(event){
     event.preventDefault()
     bookHandler(searchData())
 }
 
-const form = document.getElementById("search").addEventListener('submit',bibleContent)
+const form = document.getElementById("search").addEventListener('submit',contentDisplay)
 
 
 //Data to Store
